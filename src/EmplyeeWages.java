5378478ace96c3bc5bba1d18fe491485a2959216
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-28 08:00:21 +0530

    add daily wages for an employee

--- src/EmplyeeWages.java
+++ src/EmplyeeWages.java
@@ -11,28 +11,32 @@ public class EmplyeeWages {
 	/**
 	 * @param args
 	 */
+		// Constants
+			public static final int IS_PRESENT = 1;
+			public static final int IS_ABSENT = 2;
+			public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Variables
+				int totalHours=8;
+				int totalDays=0;
+
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		// TYPE CASTING - external
 				int employeeCheck = (int) (Math.floor(Math.random()*10)%2);
 				System.out.println("integer generated - "+employeeCheck);
 				
-				/*
-				 * Switch Case
-				 */
-				switch(employeeCheck) {
-				case 0 :
-						System.out.println("Employee is Present");
-						break;
-				case 1:
-						System.out.println("Employee is Absent");
-						break;
-				default :
-						System.out.println("employee absent");
-						break;
+				if(employeeCheck == IS_PRESENT){
+					System.out.println("Employee is part time");
+					System.out.println("Employee daily wages "+totalHours*EMPLOYEE_WAGE_PER_HOUR);
 				}
+				else {
+					System.out.println("employee not present hence no daily wages provided");
+				}	
+				
 	}
 
 }
