d0d45afd09dab566a512cd11aa75d7c90843687d
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-28 08:11:15 +0530

    add part time wages for an employee

--- src/EmplyeeWages.java
+++ src/EmplyeeWages.java
@@ -12,30 +12,37 @@ public class EmplyeeWages {
 	 * @param args
 	 */
 		// Constants
-			public static final int IS_PRESENT = 1;
-			public static final int IS_ABSENT = 2;
+			public static final int IS_FULL_TIME = 1;
+			public static final int IS_PART_TIME= 2;
 			public static final int EMPLOYEE_WAGE_PER_HOUR = 20;
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		// Variables
-				int totalHours=8;
-				int totalDays=0;
+				int totalHours=0;
+				
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		// TYPE CASTING - external
-				int employeeCheck = (int) (Math.floor(Math.random()*10)%2);
+				int employeeCheck = (int) (Math.floor(Math.random()*10)%3);
 				System.out.println("integer generated - "+employeeCheck);
 				
-				if(employeeCheck == IS_PRESENT){
+				
+				if(employeeCheck == IS_FULL_TIME) {
+					System.out.println("Employee is full time");
+					totalHours=8;
+				}
+				else if(employeeCheck == IS_PART_TIME){
 					System.out.println("Employee is part time");
-					System.out.println("Employee daily wages "+totalHours*EMPLOYEE_WAGE_PER_HOUR);
+					totalHours=4;
 				}
 				else {
-					System.out.println("employee not present hence no daily wages provided");
-				}	
+					System.out.println("employee not present");
+				}
+				
+				System.out.println("wage - "+totalHours*EMPLOYEE_WAGE_PER_HOUR);
 				
 	}
 
