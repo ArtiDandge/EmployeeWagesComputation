3cdd4a4632922628e9ed506ba16e28c8e20e54e8
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-28 07:42:00 +0530

    check employee is present or absent

--- src/EmplyeeWages.java
+++ src/EmplyeeWages.java
@@ -14,6 +14,25 @@ public class EmplyeeWages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		// TYPE CASTING - external
+				int employeeCheck = (int) (Math.floor(Math.random()*10)%2);
+				System.out.println("integer generated - "+employeeCheck);
+				
+				/*
+				 * Switch Case
+				 */
+				switch(employeeCheck) {
+				case 0 :
+						System.out.println("Employee is Present");
+						break;
+				case 1:
+						System.out.println("Employee is Absent");
+						break;
+				default :
+						System.out.println("employee absent");
+						break;
+				}
 	}
 
 }
