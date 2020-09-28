1cdb7ad82e38446fa07c7eba4d718da5079c5477
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-28 07:35:25 +0530

    display welcome message

---
+++ src/EmplyeeWages.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author HP
+ *
+ */
+public class EmplyeeWages {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
