1a506d2ee16ce6c8bc07b9d38a2fb1c1f80902e6
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-10-04 22:28:51 +0530

    wages for multiple company using interface approach

--- src/EmplyeeWages.java
+++ src/EmplyeeWages.java
@@ -1,3 +1,28 @@
+class CompanyEmpWage{
+	public final String company;
+	public final int EMPLOYEE_WAGE_PER_HOUR;
+	public final int MAX_WORKING_DAYS_PER_MONTH;
+	public final int MAX_WORKING_HOURS_PER_DAY;
+	public int totalEmpWage;
+	
+	
+	public CompanyEmpWage(String company, int EMPLOYEE_WAGE_PER_HOUR,int MAX_WORKING_DAYS_PER_MONTH, int MAX_WORKING_HOURS_PER_DAY) {
+		this.company = company;
+		this.EMPLOYEE_WAGE_PER_HOUR = EMPLOYEE_WAGE_PER_HOUR;
+		this.MAX_WORKING_DAYS_PER_MONTH = MAX_WORKING_DAYS_PER_MONTH;
+		this.MAX_WORKING_HOURS_PER_DAY = MAX_WORKING_HOURS_PER_DAY;
+	}
+	
+	public void setTotalEmpWage(int totalWage) {
+		
+		this.totalEmpWage = totalWage;
+	}
+	 public String toString() {
+ 		return "Total Employee Wage of "+company+" is :" + totalEmpWage;
+
+     }
+}
+
 public class EmplyeeWages {
 
         /**
@@ -7,28 +32,39 @@ public class EmplyeeWages {
             public static final int IS_FULL_TIME = 1;
             public static final int IS_PART_TIME= 2;
 
-			private final String company;
-			private final int EMPLOYEE_WAGE_PER_HOUR;
-			private final int MAX_WORKING_DAYS_PER_MONTH;
-			private final int MAX_WORKING_HOURS_PER_DAY;
-			private int totalEmpWage;
-			
-			public EmplyeeWages(String company, int EMPLOYEE_WAGE_PER_HOUR,int MAX_WORKING_DAYS_PER_MONTH, int MAX_WORKING_HOURS_PER_DAY) {
-				this.company = company;
-				this.EMPLOYEE_WAGE_PER_HOUR = EMPLOYEE_WAGE_PER_HOUR;
-				this.MAX_WORKING_DAYS_PER_MONTH = MAX_WORKING_DAYS_PER_MONTH;
-				this.MAX_WORKING_HOURS_PER_DAY = MAX_WORKING_HOURS_PER_DAY;
-			}
-			
+            private int numberOfCompany = 0;
+            private  CompanyEmpWage[] CompanyEmpWageArray;
+            
+            public EmplyeeWages() {
+            	
+            	CompanyEmpWageArray = new CompanyEmpWage[5];
+            	
+            }
+            
+            private void addCompanyWage(String company, int EMPLOYEE_WAGE_PER_HOUR, int MAX_WORKING_DAYS_PER_MONTH,int MAX_WORKING_HOURS_PER_DAY) {
+            	
+            	CompanyEmpWageArray[numberOfCompany] = new CompanyEmpWage(company, EMPLOYEE_WAGE_PER_HOUR,  MAX_WORKING_DAYS_PER_MONTH,MAX_WORKING_HOURS_PER_DAY);
+            	numberOfCompany++;
+            }
+            
+            private void computeEmpWage() {
+            	
+            	for(int i = 0; i < numberOfCompany; i++) {
+            		
+            		CompanyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(CompanyEmpWageArray[i]));
+            		System.out.println(CompanyEmpWageArray[i]);
+            	}
+            }
            //get wages from class method
-            public void computeEmpWage(){
+            private int computeEmpWage(CompanyEmpWage companyEmpWage){
                 //variables
                 int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-                System.out.println("------------------------------------------------");
-                System.out.println("Wages for "+ company);
+//                System.out.println("------------------------------------------------");
+//                System.out.println("Wages for "+ company);
 
                 //calculate wages till max condition given
-                while (totalEmpHrs <= MAX_WORKING_DAYS_PER_MONTH && totalWorkingDays < MAX_WORKING_HOURS_PER_DAY){
+                while (totalEmpHrs <= companyEmpWage.MAX_WORKING_DAYS_PER_MONTH 
+                		&& totalWorkingDays < companyEmpWage.MAX_WORKING_HOURS_PER_DAY){
                 totalWorkingDays++;
                 //employee presence check
                 int empCheck = (int) Math.floor(Math.random()*10)%3;
@@ -48,24 +84,25 @@ public class EmplyeeWages {
                 System.out.println("Day : " + totalWorkingDays + ", Emp Hours : "+empHrs);
                     }
                 //compute and display total employee wages
-                int totalEmpWage = totalEmpHrs * EMPLOYEE_WAGE_PER_HOUR;
+                return  totalEmpHrs * companyEmpWage.EMPLOYEE_WAGE_PER_HOUR;
        
             }
 
-            public String toString() {
-        		return "Total Employee Wage of "+company+"is :" + totalEmpWage;
-
-            }
+           
             public static void main(String[] args) {
                 // TODO Auto-generated method stub
                 System.out.println("Welcome to Employee Wage Computation Program");
-                EmplyeeWages Relience = new EmplyeeWages("Relience", 22, 10, 7);
-                EmplyeeWages BigBazaj = new EmplyeeWages("BigBazar", 12, 8, 25);
-                System.out.println(Relience);
-                Relience.computeEmpWage();
-                BigBazaj.computeEmpWage();
-
+//                EmpWageBuilderArray Relience = new EmpWageBuilderArray("Relience", 22, 10, 7);
+//                EmpWageBuilderArray BigBazaj = new EmpWageBuilderArray("BigBazar", 12, 8, 25);
+//                System.out.println(Relience);
+//                Relience.computeEmpWage();
+//                BigBazaj.computeEmpWage();
+                	EmplyeeWages empWageBuilderArray = new EmplyeeWages();
+                	empWageBuilderArray.addCompanyWage("Dmart",20, 2, 10);
+                	empWageBuilderArray.addCompanyWage("Relience", 10, 4, 20);
+                	empWageBuilderArray.computeEmpWage();
             }
 
 }
+
 
